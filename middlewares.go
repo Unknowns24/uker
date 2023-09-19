9c4bc2e8dae2e15b6615cd762205064bb4a9eb04
unknowns <unknowns0074@gmail.com>
2023-09-19 19:12:52 -0300

    Implemented two new middlewares to verify if user has permissions

--- middlewares.go
+++ middlewares.go
@@ -1,11 +1,13 @@
 package uker
 
 import (
+	"context"
 	"strconv"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v4"
+	"github.com/unknowns24/uker/proto"
 )
 
 // middleware contants
@@ -36,6 +38,24 @@ type middlewares interface {
 	//
 	// @return error: error on authentication
 	IsAuthenticated(c *fiber.Ctx) error
+
+	// Middleware to validate if user has required permissions
+	//
+	// @param authService proto.AuthServiceClient: authService with stablished connection to make the request.
+	//
+	// @param permissions []string: Array with the required permissions that user needs to have.
+	//
+	// @return func(c *fiber.Ctx) error: fiber middleware function to use on the
+	HasPermissions(authService proto.AuthServiceClient, permissions []string) func(c *fiber.Ctx) error
+
+	// Middleware to validate if user has at least one of the required permissions
+	//
+	// @param authService proto.AuthServiceClient: authService with stablished connection to make the request.
+	//
+	// @param permissions []string: Array with the required permissions that user needs to have.
+	//
+	// @return func(c *fiber.Ctx) error: fiber middleware function to use on the
+	HasAtLeastOnePermission(authService proto.AuthServiceClient, permissions []string) func(c *fiber.Ctx) error
 }
 
 // Local struct to be implmented
@@ -89,4 +109,64 @@ func (m *middlewares_implementation) GenerateJWT(id uint, keeplogin bool) (strin
 	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, payload).SignedString([]byte(jwt_key))
 
 	return token, err
+}
+
+func (m *middlewares_implementation) HasPermissions(authService proto.AuthServiceClient, permissions []string) func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		// Get userId from context
+		userId := c.Context().UserValue(CONTEXT_VALUE_USERID).(uint)
+
+		if userId == 0 {
+			return endOutPut(c, fiber.StatusUnauthorized, ERROR_MIDDLEWARE_NO_AUTHENTICATED_USER, nil)
+		}
+
+		// Validate if user have every permission
+		for _, perm := range permissions {
+			havePermRes, err := authService.HavePermission(context.Background(), &proto.HavePermReq{
+				UserId:     uint64(userId),
+				Permission: perm,
+			})
+
+			//TODO: check the way HavePermissions return error
+			if err != nil {
+				return c.SendString(err.Error())
+			}
+
+			if !havePermRes.HavePermission {
+				return endOutPut(c, fiber.StatusBadRequest, ERROR_MIDDLEWARE_INSUFICIENT_PERMISSIONS, nil)
+			}
+		}
+
+		return c.Next()
+	}
+}
+
+func (m *middlewares_implementation) HasAtLeastOnePermission(authService proto.AuthServiceClient, permissions []string) func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		// Get userId from context
+		userId := c.Context().UserValue(CONTEXT_VALUE_USERID).(uint)
+
+		if userId == 0 {
+			return endOutPut(c, fiber.StatusUnauthorized, ERROR_MIDDLEWARE_NO_AUTHENTICATED_USER, nil)
+		}
+
+		// Validate if user have at least one permission
+		for _, perm := range permissions {
+			havePermRes, err := authService.HavePermission(context.Background(), &proto.HavePermReq{
+				UserId:     uint64(userId),
+				Permission: perm,
+			})
+
+			//TODO: check the way HavePermissions return error
+			if err != nil {
+				return c.SendString(err.Error())
+			}
+
+			if havePermRes.HavePermission {
+				return c.Next()
+			}
+		}
+
+		return endOutPut(c, fiber.StatusBadRequest, ERROR_MIDDLEWARE_INSUFICIENT_PERMISSIONS, nil)
+	}
 }
