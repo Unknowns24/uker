f74c0c40d580d5e3ade1b9670021f3fdf8d88479
unknowns <unknowns0074@gmail.com>
2023-09-19 14:36:08 -0300

    Implemented new helper functions for strings

--- strings.go
+++ strings.go
@@ -5,11 +5,15 @@ import "strings"
 // Global interface
 type str interface {
 	Contains(str string, chars []string) bool
+	SanitizeString(str string) string
 	HasNoValidChars(str string) bool
 }
 
 // Local struct to be implmented
 type str_implementation struct{}
+
+// local variables
+var invalidChars = [...]string{"*", "/", "\\", "=", ">", "<", ":", ";", "¿", "?", "\"", "|", "!", "¡", "º"}
 
 // External contructor
 func Str() str {
@@ -27,8 +31,7 @@ func (s *str_implementation) Contains(str string, chars []string) bool {
 }
 
 func (s *str_implementation) HasNoValidChars(str string) bool {
-	invalidChars := []string{"*", "/", "\\", "=", ">", "<", ":", ";", "¿", "?", "\"", "|", "!", "¡", "º"}
-
+	// Check if has no valid characters
 	for _, char := range invalidChars {
 		if strings.Contains(str, char) {
 			return true
@@ -36,4 +39,20 @@ func (s *str_implementation) HasNoValidChars(str string) bool {
 	}
 
 	return false
+}
+
+func (s *str_implementation) SanitizeString(str string) string {
+	parsedStr := str
+
+	// Remove all invalid characters
+	for _, char := range invalidChars {
+		if strings.Contains(str, char) {
+			parsedStr = strings.ReplaceAll(parsedStr, char, "")
+		}
+	}
+
+	// Remove all spaces
+	parsedStr = strings.ToLower(strings.ReplaceAll(parsedStr, " ", "_"))
+
+	return parsedStr
 }
