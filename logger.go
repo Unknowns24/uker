91636f97e30fddd8b3b0333c868dc5adf9b65229
Unknowns24 <unknowns0074@gmail.com>
2024-07-29 13:51:44 -0300

    fix: Fixed connection problems
    Fixed connection lost and implemented a recover system by testing connection with dynamic time

--- logger.go
+++ logger.go
@@ -2,24 +2,34 @@ package uker
 
 import (
 	"encoding/json"
+	"fmt"
+	"time"
 
 	"github.com/fluent/fluent-logger-golang/fluent"
 	"github.com/sirupsen/logrus"
 )
 
 type fluentdWriter struct {
-	tag    string
-	logger *fluent.Fluent
+	conn   *fluent.Fluent
+	config *LoggerConfig
 }
 
-func (f *fluentdWriter) Write(p []byte) (n int, err error) {
+func (fw *fluentdWriter) Write(p []byte) (n int, err error) {
 	var data map[string]interface{}
 	err = json.Unmarshal(p, &data)
 	if err != nil {
 		return 0, err
 	}
 
-	err = f.logger.Post(f.tag, data)
+	// Adding metadata fields if not empty inside data struct
+	metadataFields := map[string]string{"application": fw.config.Application, "servicename": fw.config.ServiceName, "source": fw.config.Source}
+	for label, value := range metadataFields {
+		if value != "" {
+			data[label] = value
+		}
+	}
+
+	err = fw.conn.Post(fw.config.Tag, data)
 	if err != nil {
 		return 0, err
 	}
@@ -27,23 +37,85 @@ func (f *fluentdWriter) Write(p []byte) (n int, err error) {
 }
 
 type LoggerConfig struct {
-	LogFormatter     logrus.Formatter
-	FluentPostTag    string
-	FluentConnection *fluent.Fluent
+	FluentConfig       fluent.Config
+	LogFormatter       logrus.Formatter
+	TestConnectionTime time.Duration
+
+	// Post metadata info
+	Tag         string
+	Source      string
+	ServiceName string
+	Application string
 }
 
-func NewLogger(c LoggerConfig) *logrus.Logger {
+type UkerLogger struct {
+	config *LoggerConfig
+	writer *fluentdWriter
+	Logger *logrus.Logger
+}
+
+func NewLogger(c *LoggerConfig) *UkerLogger {
 	log := logrus.New()
+
+	// Create a Fluentd logger
+	fluentInstance, err := fluent.New(c.FluentConfig)
+
+	if err != nil {
+		panic(fmt.Sprintf("failed to connect to Fluentd: %v", err))
+	}
 
 	// Create a FluentdWriter
 	fluentWriter := &fluentdWriter{
-		tag:    c.FluentPostTag,
-		logger: c.FluentConnection,
+		conn:   fluentInstance,
+		config: c,
 	}
 
 	// Set up logrus to use the FluentdWriter
 	log.SetFormatter(c.LogFormatter)
 	log.SetOutput(fluentWriter)
 
-	return log
+	uLogger := &UkerLogger{
+		config: c,
+		writer: fluentWriter,
+		Logger: log,
+	}
+
+	go uLogger.monitorConnection(c.TestConnectionTime)
+
+	return uLogger
+}
+
+func (u *UkerLogger) checkConnection() bool {
+	// Enviar un mensaje de prueba a Fluentd para verificar la conexión
+	err := u.writer.conn.Post("test", map[string]string{"message": "ping"})
+	return err == nil
+}
+
+func (u *UkerLogger) stablishFluentDConnection() error {
+	fluentInstance, err := fluent.New(u.config.FluentConfig)
+
+	if err != nil {
+		return err
+	}
+
+	u.writer.conn = fluentInstance
+	return nil
+}
+
+func (u *UkerLogger) reconnectFluentD() error {
+	// Intentar restablecer la conexión
+	if u.writer.conn != nil {
+		u.writer.conn.Close()
+	}
+	return u.stablishFluentDConnection()
+}
+
+func (u *UkerLogger) monitorConnection(interval time.Duration) {
+	for {
+		time.Sleep(interval)
+		if u.writer.conn == nil || !u.checkConnection() {
+			fmt.Println("Connection to FluentD lost. Attempting to reconnect...")
+			RetryWithBackoff(u.reconnectFluentD, "Failed to reconnect, retrying in: ", "Reconnected successfully", "seconds")
+		}
+	}
 }
